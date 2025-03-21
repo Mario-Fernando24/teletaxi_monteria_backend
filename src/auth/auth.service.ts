e3629eb5e36d4cc279c8e83a0892b2594c75e7c4
Mario-Fernando24 <3115408069.mafe@gmail.com>
2025-03-20 23:49:40 -0500

    validar phone y email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
@@ -12,7 +12,22 @@ export class AuthService {
     }
 
 
-    register(user: RegisterUserDto){
+   async register(user: RegisterUserDto){
+
+        //Metodo (findOneBy) nos ayuda buscar un usuario atravez de un campo especifico
+        const emailExist = await this.userRepository.findOneBy({email: user.email})
+        
+        if(emailExist){
+            //409
+            return new HttpException('El email ya esta registrado', HttpStatus.CONFLICT)
+        }
+        const phoneExist = await this.userRepository.findOneBy({phone: user.phone})
+        
+        if(phoneExist){
+            //409
+            return new HttpException('El Telefono ya esta registrado', HttpStatus.CONFLICT)
+        }
+        
         const newUserRegister = this.userRepository.create(user);
         return this.userRepository.save(newUserRegister);
     }
